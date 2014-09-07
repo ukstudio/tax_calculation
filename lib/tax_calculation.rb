93b46b6bd2be95747aae37c608bc0bafd2fb1b6f
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2014-09-07 20:35:13 +0900

    Round tax and total price

--- lib/tax_calculation.rb
+++ lib/tax_calculation.rb
@@ -15,12 +15,30 @@ class TaxCalculation
   end
 
   def total_price
-    tmp = @items.inject(0) {|total, item|
-      total += item.price * item.quantity
-    }
-    tax_inclusive? ? tmp : tmp * multiplier_rate
+    round(total_price_without_round, 0)
   end
 
+  def total_price_without_round
+    tmp = @items.inject(0) {|total, item| total += item.price * item.quantity }
+    tax_inclusive? ? BigDecimal(tmp.to_s) : tmp * multiplier_rate
+  end
+
+  def tax
+    round(tax_without_round, 0)
+  end
+
+  def tax_without_round
+    if tax_inclusive?
+      total_price / multiplier_rate * rate
+    else
+      total_price - (@items.inject(0) {|total, item| total += item.price * item.quantity })
+    end
+  end
+
+  def round(num, scale)
+    BigDecimal(num.to_s).round(0, BigDecimal::ROUND_DOWN)
+  end
+  
   def multiplier_rate
     1 + rate
   end
--- spec/lib/tax_calculation_spec.rb
+++ spec/lib/tax_calculation_spec.rb
@@ -2,13 +2,62 @@ require 'spec_helper'
 
 describe TaxCalculation do
   describe '#total_price' do
-    let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: true)}
-    before do
-      tax_calculation.items << TaxCalculation::Item.new(price: 1080, quantity: 1)
+    context 'tax_inclusive = true' do
+      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: true)}
+
+      before do
+        tax_calculation.items << TaxCalculation::Item.new(price: 1080, quantity: 3)
+        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
+        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
+      end
+
+      subject { tax_calculation.total_price }
+
+      it { is_expected.to eq 7256 }
     end
 
-    subject { tax_calculation.total_price }
+    context 'tax_inclusive = false' do
+      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: false)}
 
-    it { is_expected.to eq 1080 }
+      before do
+        tax_calculation.items << TaxCalculation::Item.new(price: 1000, quantity: 3)
+        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
+        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
+      end
+
+      subject { tax_calculation.total_price }
+
+      it { is_expected.to eq 7577 }
+    end
+  end
+
+  describe '#tax' do
+    context '#tax_inclusive = true' do
+      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: true)}
+
+      before do
+        tax_calculation.items << TaxCalculation::Item.new(price: 1080, quantity: 3)
+        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
+        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
+      end
+
+      subject { tax_calculation.tax}
+
+      it { is_expected.to eq 537 }
+    end
+
+    context '#tax_inclusive = false' do
+      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: false)}
+
+      before do
+        tax_calculation.items << TaxCalculation::Item.new(price: 1000, quantity: 3)
+        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
+        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
+      end
+
+      subject { tax_calculation.tax}
+
+      it { is_expected.to eq 561 }
+    end
   end
 end
