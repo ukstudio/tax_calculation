744036f1ae801fc2fa4c33aa333ca10a3d7580cd
AKAMATSU Yuki <y.akamatsu@ukstudio.jp>
2014-09-07 20:38:54 +0900

    Refactoring spec

--- spec/lib/tax_calculation_spec.rb
+++ spec/lib/tax_calculation_spec.rb
@@ -1,63 +1,33 @@
 require 'spec_helper'
 
 describe TaxCalculation do
-  describe '#total_price' do
-    context 'tax_inclusive = true' do
-      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: true)}
+  context 'tax_inclusive = true' do
+    let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: true)}
 
-      before do
-        tax_calculation.items << TaxCalculation::Item.new(price: 1080, quantity: 3)
-        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
-        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
-      end
-
-      subject { tax_calculation.total_price }
-
-      it { is_expected.to eq 7256 }
+    before do
+      tax_calculation.items << TaxCalculation::Item.new(price: 1080, quantity: 3)
+      tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
+      tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
     end
 
-    context 'tax_inclusive = false' do
-      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: false)}
+    subject { tax_calculation }
 
-      before do
-        tax_calculation.items << TaxCalculation::Item.new(price: 1000, quantity: 3)
-        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
-        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
-      end
-
-      subject { tax_calculation.total_price }
-
-      it { is_expected.to eq 7577 }
-    end
+    it { expect(subject.total_price).to eq 7256 }
+    it { expect(subject.tax).to eq 537 }
   end
 
-  describe '#tax' do
-    context '#tax_inclusive = true' do
-      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: true)}
+  context 'tax_inclusive = false' do
+    let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: false)}
 
-      before do
-        tax_calculation.items << TaxCalculation::Item.new(price: 1080, quantity: 3)
-        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
-        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
-      end
-
-      subject { tax_calculation.tax}
-
-      it { is_expected.to eq 537 }
+    before do
+      tax_calculation.items << TaxCalculation::Item.new(price: 1000, quantity: 3)
+      tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
+      tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
     end
 
-    context '#tax_inclusive = false' do
-      let(:tax_calculation) { TaxCalculation.new(rate: 0.08, tax_inclusive: false)}
+    subject { tax_calculation }
 
-      before do
-        tax_calculation.items << TaxCalculation::Item.new(price: 1000, quantity: 3)
-        tax_calculation.items << TaxCalculation::Item.new(price: 20,   quantity: 1)
-        tax_calculation.items << TaxCalculation::Item.new(price: 999,  quantity: 4)
-      end
-
-      subject { tax_calculation.tax}
-
-      it { is_expected.to eq 561 }
-    end
+    it { expect(subject.total_price).to eq 7577 }
+    it { expect(subject.tax).to eq 561 }
   end
 end
